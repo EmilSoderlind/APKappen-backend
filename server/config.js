d23cf796cdd175c244506c45a0df4e94f90a0552
Emil Söderlind <id15esd@cs.umu.se>
2019-09-25 23:55:31 +0200

    further refactoring

---
+++ server/config.js
@@ -0,0 +1,3 @@
+
+// systembolagets API
+exports.Ocp_Apim_Subscription_Key = "269a31c61c6640dcaf4df2e618698595";
--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ var request = require('request');
 var slugify = require('slugify')
 var fs = require("fs");
 
-var startDate = new Date()
 
 const express = require('express')
 const app = express()
 const port = 1337
+const systembolagetAPIEndpoint = "https://api-extern.systembolaget.se/product/v1/product";
 
+var config = require('./config');
 
-var articleList = "";
+var processedProductsList = "";
 
 
 // Create and set .URL attribute in article JSON-objects
@@ -18,134 +19,52 @@ function addURLtoProduct(product){
   var baseURL = "https:\//www.systembolaget.se/dryck";
   var categoryURL = "";
   var nameURL = "";
-  var numberURL = product.nr;
+  var numberURL = product.ProductNumber;
+
+  if(product.Category == null){
+    return;
+  }
 
   // Get category-url-text
-  switch(product.Varugrupp.toLowerCase()) {
-  case "rött vin":
+  switch(String(product.Category)) {
+  case "Röda viner":
     categoryURL = "roda-viner"
     break;
-  case "blanddrycker":
+  case "Cider och blanddrycker":
     categoryURL = "cider-och-blanddrycker"
     break;
-  case "cider":
-    categoryURL = "cider-och-blanddrycker"
-    break;
-  case "vitt vin":
+  case "Vita viner":
     categoryURL = "vita-viner"
     break;
-  case "glögg och glühwein":
-    categoryURL = "aperitif-dessert";
-    break;
-  case "öl":
-    categoryURL = "ol";
-    break;
-  case "rosévin":
-    categoryURL = "roseviner";
-    break;
-  case "blandlådor vin":
-    categoryURL = "aperitif-dessert";
-    break;
-  case "smaksatt sprit":
+  case "Sprit":
     categoryURL = "sprit";
     break;
-  case "sake":
-    categoryURL = "aperitif-dessert";
-    break;
-  case "gin och genever":
-    categoryURL = "sprit";
-    break;
-  case "whisky":
-    categoryURL = "sprit";
-    break;
-  case "rom":
-    categoryURL = "sprit";
-    break;
-  case "aperitif och dessert":
-    categoryURL = "aperitif-dessert";
-    break;
-  case "vermouth":
-    categoryURL = "aperitif-dessert";
-    break;
-  case "likör":
-    categoryURL = "sprit";
-    break;
-  case "mousserande vin":
+  case "Mousserande viner":
     categoryURL = "mousserande-viner";
     break;
-  case "armagnac och brandy":
-    categoryURL = "sprit";
+  case "Öl":
+    categoryURL = "ol";
     break;
-  case "akvavit":
-    categoryURL = "sprit";
-    break;
-  case "punsch":
-    categoryURL = "sprit";
-    break;
-  case "anissprit":
-    categoryURL = "sprit";
-    break;
-  case "akvavit och kryddat brännvin":
-    categoryURL = "sprit";
-    break;
-  case "alkoholfritt":
-    categoryURL = "alkoholfritt";
-    break;
-  case "cognac":
-    categoryURL = "sprit";
-    break;
-  case "tequila och mezcal":
-    categoryURL = "sprit";
-    break;
-  case "drinkar och cocktails":
-    categoryURL = "sprit";
-    break;
-  case "calvados":
-    categoryURL = "sprit";
-    break;
-  case "grappa och marc":
-    categoryURL = "sprit";
-    break;
-  case "bitter":
-    categoryURL = "sprit";
-    break;
-  case "vodka och brännvin":
-    categoryURL = "sprit";
-    break;
-  case "frukt och druvsprit":
-    categoryURL = "sprit";
-    break;
-  case "blå mousserande":
-    categoryURL = "aperitif-dessert";
-    break;
-  case "rosé - lägre alkoholhalt":
+  case "Roséviner":
     categoryURL = "roseviner";
     break;
-  case "vita - lägre alkoholhalt":
-    categoryURL = "vita-viner";
+  case "Presentartiklar":
+    categoryURL = "presentartiklar"
     break;
-  case "röda - lägre alkoholhalt":
-    categoryURL = "roda-viner";
+  case "Aperitif & dessert":
+    categoryURL = "aperitif-dessert"
     break;
-  case "sprit av flera typer":
-    categoryURL = "sprit";
-    break;
-  case "blå stilla":
-    categoryURL = "aperitif-dessert";
-    break;
-  case "presentförpackningar":
-    categoryURL = "presentartiklar";
-    break;
-  case "dryckestillbehör":
-    categoryURL = "presentartiklar";
+  case "Alkoholfritt":
+    categoryURL = "alkoholfritt"
     break;
   default:
-    console.log("Found new category type:" + product.Varugrupp.toLowerCase())
+    console.log("Found new category: " + product.Category)
+    console.log(product)
     // code block
 }
 
   // Get name-url-text
-  nameURL = product.Namn.toString().toLowerCase();
+  nameURL = product.ProductNameBold.toString().toLowerCase();
   nameURL = nameURL.replaceAll("\'","")
   nameURL = nameURL.replaceAll("!","")
   nameURL = slugify(nameURL);
@@ -164,6 +83,13 @@ function processParsedProducts(productList){
   var maxAPKFound = 0;
 
   for (var i = 0; i < productList.length; i++) {
+
+    // Removing products that is "IsCompletelyOutOfStock = true"
+    if(productList[i].IsCompletelyOutOfStock){
+      productList.splice(i,1);
+      console.log("Removed a IsCompletelyOutOfStock=true")
+    }
+
     addURLtoProduct(productList[i])
     addAPKtoProduct(productList[i])
 
@@ -182,10 +108,10 @@ function processParsedProducts(productList){
 
 function addAPKtoProduct(product) {
 
-  var price = parseFloat(product.Prisinklmoms);
-  var volume = parseFloat(product.Volymiml);
-  var alcohol = parseFloat(String(product.Alkoholhalt).replace("%",""));
-  var pant = product.Pant;
+  var price = parseFloat(product.Price);
+  var volume = parseFloat(product.Volume);
+  var alcohol = parseFloat(String(product.AlcoholPercentage).replace("%",""));
+  var pant = product.RecycleFee;
 
   if(Number.isNaN(price) || Number.isNaN(volume) || Number.isNaN(alcohol)){
     console.error("---------------------")
@@ -200,9 +126,9 @@ function addAPKtoProduct(product) {
   product.APK = ((alcohol/100)*volume)/price;
 
   if(pant == undefined){
-    product.APKMedPant = product.APK
+    product.APKWithPant = product.APK
   }else{
-    product.APKMedPant = ((alcohol/100)*volume)/(price + parseFloat(pant));
+    product.APKWithPant = ((alcohol/100)*volume)/(price + parseFloat(pant));
   }
 }
 
@@ -219,32 +145,28 @@ String.prototype.replaceAll = function(str1, str2, ignore){
 }
 
 function parseSystembolagetsAPI(){
-  console.log("Parsing from SB-API starting.")
+  console.log("Parsing from systembolagets API")
 
-  request('https://www.systembolaget.se/api/assortment/products/xml', function (error, response, body) {
+  headers = {
+   "Ocp-Apim-Subscription-Key" : config.Ocp_Apim_Subscription_Key
+  };
+
+
+  request({ url: systembolagetAPIEndpoint, headers: headers }, function (error, response, body) {
+
       if (!error && response.statusCode == 200) {
-        console.info('Download time: %dms', new Date() - startDate)
+
+        var parsedProducts = JSON.parse(body);
+        processParsedProducts(parsedProducts)
 
         var start = new Date()
-        let resultJSON = body;
-        console.info('XML to JSON parse time: %dms', new Date() - start)
-
-        articleList = resultJSON["artiklar"]["artikel"];
-
-        // Remove ":s from number attributes
-
-        var start = new Date()
-        processParsedProducts(articleList)
-
-        var start = new Date()
-        articleList.sort(function(a, b) {
+        parsedProducts.sort(function(a, b) {
           return parseFloat(b.APK) - parseFloat(a.APK);
         });
 
-        console.info('Sorting time: %dms', new Date() - start)
-        console.info('APK + URL processing time: %dms', new Date() - start)
-        console.info('Total parse time: %dms', new Date() - startDate)
-        console.log("DONE\nAntal produkter: " + Object.keys(articleList).length + "\n")
+        console.log("DONE\nAntal produkter: " + Object.keys(parsedProducts).length + "\n")
+
+        processedProductsList = parsedProducts;
 
       }else{
         console.log("ERROR: \n" + response.statusCode + "-" + error)
@@ -260,8 +182,8 @@ function openEndPoints(){
     res.set('Content-Type', 'text/html');
     var listHtml = "<!DOCTYPE html><html lang=\"en\"><head><title>APK DUMP</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"https:\/\/maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\"><script src=\"https:\//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script><script src=\"https:\//maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js\"></script></head><body>";
     for(var i = 0; i<18000; i++){ //TODO 18000 som magic number? använd nån längd av nått
-      var prod = articleList[i];
-      listHtml = listHtml + "<li class=\"list-group-item\">"+ (i+1) +". "+ prod.Namn +" " + prod.APKScore + " APK-Score (1-100)  <a href="+addURLtoProduct(prod)+">"+addURLtoProduct(prod)+"</a></li>"
+      var prod = processedProductsList[i];
+      listHtml = listHtml + "<li class=\"list-group-item\">"+ (i+1) +". "+ prod.ProductNameBold +" " + prod.APKScore + " APK-Score (1-100)  <a href="+addURLtoProduct(prod)+">"+addURLtoProduct(prod)+"</a></li>"
     }
     res.send('<div class=\"container\"><h2>TOP APK</h2><ul class=\"list-group\">' + listHtml + '</ul></div></body></html>');
   })
@@ -272,23 +194,23 @@ function openEndPoints(){
 
   // Return all articles from ARRAY (memory)
   app.get('/APKappen_v1/', (req, res) => {
-    if(articleList == undefined){
+    if(processedProductsList == undefined){
       res.sendStatus(204)
     }else{
       var start = new Date()
-      res.json(articleList)
+      res.json(processedProductsList)
       console.info('Array | Response time: %dms', new Date() - start)
     }
   })
 
   // Return top :numberOfArticles from ARRAY (memory)
   app.get('/APKappen_v1/:numberOfArticles', (req, res) => {
-    if(articleList == undefined){
+    if(processedProductsList == undefined){
       res.sendStatus(204)
     }else{
       var start = new Date()
-      res.json(articleList.slice(0, req.params.numberOfArticles))
-      console.info('Array | #articles: '+ req.params.numberOfArticles +' | Response time: %dms', new Date() - start)
+      res.json(processedProductsList.slice(0, req.params.numberOfArticles))
+      console.info('#articles: '+ req.params.numberOfArticles +' | Response time: %dms', new Date() - start)
     }
 
   })
