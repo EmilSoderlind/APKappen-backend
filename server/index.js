8a05470d35393f1d8e7ae927ae357f8496e60060
Emil Söderlind <id15esd@cs.umu.se>
2019-10-04 00:13:05 +0200

    dns update

--- server/index.js
+++ server/index.js
@@ -272,14 +272,18 @@ function reparseSystembolagetAPI(){
 }
 
 function updateDynamicDns(){
-  console.log("Updating dns.")
+  console.log("Updating dns")
 
   let dnsToken = secret.duckdnsToken;
 
   request("https://www.duckdns.org/update?domains=skruvdragarn&token=" + dnsToken, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+
+    if (!error && response.statusCode == 200 && body == "OK") {
+      console.log("Updating dns - DONE")
+    }else{
+      console.log("Could not update DNS. ERROR: \n" + response.statusCode + "-" + error)
+    }
+
 });
 }
 
