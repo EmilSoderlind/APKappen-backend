e8d2ba7e7f09d934223280fc98ac1f683cea13b4
Emil Söderlind <soderlindemil@gmail.com>
2021-02-27 17:18:17 +0100

    Adding pagination

--- Experiment/api_endpoints.js
+++ Experiment/api_endpoints.js
@@ -7,8 +7,23 @@ const PORT = 7000
 const app = express()
 
 app.get('/', (req, res) => {
-    db.find({}).sort({ APK: -1 }).limit(30).exec(function (err, docs) {
+
+    let store = (req.query.store) || null
+    let category = (req.query.category) || undefined
+    let postsPerPage = Number(req.query.postsPerPage) || 5 // If you send postsPerPage=5 you get 5 hehe
+    let pageIndex = Number(req.query.pageIndex) || 0
+    let search = req.query.search || null
+
+    let queryObject = {}
+
+    //console.log('category_mapping[category] = ' + JSON.stringify(category_mapping[category]))
+
+    //if (category && category_mapping[category]) queryObject['categoryLevel1'] = category
+    //if (search) queryObject['categoryLevel1'] = category
+
+    db.find(queryObject).sort({ APK: -1 }).skip(pageIndex*postsPerPage).limit(postsPerPage).exec(function (err, docs) {
         res.send(docs)
     });
 })
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!\n`))
